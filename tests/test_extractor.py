cbe77e767e483ca4c3a247a405c6632494f42d19
nosoop <nosoop@users.noreply.github.com>
2024-10-24 04:50:11 -0700

    Apply ruff format to tests

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-import pytest
 import moombox.extractor
+import pytest
 
-@pytest.mark.parametrize("expected,input",[
-    ("dQw4w9WgXcQ", "dQw4w9WgXcQ"),
-    ("dQw4w9WgXcQ", "https://youtu.be/dQw4w9WgXcQ"),
-    ("dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
-    ("dQw4w9WgXcQ", "https://www.youtube.com/live/dQw4w9WgXcQ"),
-    ("dQw4w9WgXcQ", "https://www.youtube.com/shorts/dQw4w9WgXcQ"),
-])
+
+@pytest.mark.parametrize(
+    "expected,input",
+    [
+        ("dQw4w9WgXcQ", "dQw4w9WgXcQ"),
+        ("dQw4w9WgXcQ", "https://youtu.be/dQw4w9WgXcQ"),
+        ("dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+        ("dQw4w9WgXcQ", "https://www.youtube.com/live/dQw4w9WgXcQ"),
+        ("dQw4w9WgXcQ", "https://www.youtube.com/shorts/dQw4w9WgXcQ"),
+    ],
+)
 def test_video_id_extraction(input: str, expected: str):
     assert expected == moombox.extractor.extract_video_id_from_string(input)
