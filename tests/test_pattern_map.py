5daf4dd0b35ee7607d3516acd6194259b3f9135a
nosoop <nosoop@users.noreply.github.com>
2024-10-24 04:41:44 -0700

    Implement RSS feed monitoring

---
+++ tests/test_pattern_map.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import re
+
+import pytest
+from moombox.config import PatternMap
+from moombox.feed_monitor import get_pattern_matches
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        # ensure exotic unicode gets substituted with ASCII
+        (
+            "【UNARCHIVE KARAOKE】TO ALL THE LONELY BUT LOVELY AXELOTLS OUT THERE♡【𝐑𝐄𝐁𝐑𝐎𝐀𝐃𝐂𝐀𝐒𝐓】",
+            {
+                "rebroadcast",
+                "karaoke",
+                "unarchived",
+            },
+        ),
+        # spaces used in between single characters
+        (
+            "【K A R A O K E】rock",
+            {
+                "karaoke",
+            },
+        ),
+    ],
+)
+def test_patterns(input: str, expected: set):
+    test_pattern: PatternMap = {
+        "unarchived": re.compile(r"(?i)(\W|^)unar?chived?"),
+        "karaoke": re.compile(r"(?i)(\W|^)karaoke"),
+        "rebroadcast": re.compile(r"(?i)(\W|^)re-?broadcast"),
+    }
+    assert expected == get_pattern_matches(test_pattern, input)
